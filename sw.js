9b0abcd7a777f6cbc3f1270868a0ad9b26f4f548
ykhymj1 <91314076+ykhymj1@users.noreply.github.com>
2026-07-31 12:29:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 여행 일정 네비 PWA - Service Worker */
-var CACHE = "wtrip-v20.2-20260730";
+var CACHE = "wtrip-v20.3-20260731";
 var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", function (e) {
